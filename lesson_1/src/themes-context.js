1d389ec0db8af69df2257cce36a7d879ef15fd45
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-03-20 19:37:08 +0200

    add

--- lesson_1/src/themes-context.js
+++ lesson_1/src/themes-context.js
@@ -10,4 +10,4 @@ export const themes = {
     background: '#eeeeee',
   },
 };
-export const ThemeContext = React.createContext(themes.light);
+export const ThemeContext = React.createContext(themes.dark);
