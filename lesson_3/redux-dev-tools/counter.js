3388227f23587178c334699fd9818ea1f01f65d1
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-03-24 12:08:56 +0200

    add

---
+++ lesson_3/redux-dev-tools/counter.js
@@ -0,0 +1,30 @@
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const increment = () => {
+  return {
+    type: 'COUNTER/INCREMENT',
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: 'COUNTER/DECREMENT',
+  };
+};
+
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
