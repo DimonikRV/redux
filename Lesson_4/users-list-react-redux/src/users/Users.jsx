cd1c9c2b95def7dd1780da91d350f652e067be46
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-03-27 14:02:38 +0300

    add

---
+++ Lesson_4/users-list-react-redux/src/users/Users.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as userActions from './users.actions';
+
+const Users = ({ users, createUser, deleteUser }) => {
+  const onUserCreate = () => {
+    const id = Math.round(Math.random() * 1000000);
+    const newUser = {
+      id,
+      name: `User # ${id}`,
+    };
+    createUser(newUser);
+  };
+
+  return (
+    <div className="users">
+      <button className="users__create-btn" onClick={onUserCreate}>
+        Create user
+      </button>
+      <ul className="users__list">
+        {users.map(user => (
+          <li key={user.id} className="users__list-item">
+            {user.name}
+            <button className="users__delete-btn" onClick={() => deleteUser(user.id)}>
+              +
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    users: state.users.usersList,
+  };
+};
+const mapDispatch = {
+  createUser: userActions.addUser,
+  deleteUser: userActions.deleteUser,
+};
+
+const connector = connect(mapState, mapDispatch);
+const ConnectUsers = connector(Users);
+
+export default ConnectUsers;
